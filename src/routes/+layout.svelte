be7869c9e5650356babeef8dc0f3a95cc71ca50a
QuantumKya <rohan.vats4nier@gmail.com>
2025-08-22 13:18:06 -0500

    return to home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/state';
 	
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>{page.url.pathname} - quantumkya.dev</title>
 </svelte:head>
+
+{#if page.url.pathname !== '/'}
+<div class="bg-green-600 absolute z-10 right-2"><a href="/">&lt;&lt; Return to Home</a></div>
+{/if}
 
 {@render children?.()}
