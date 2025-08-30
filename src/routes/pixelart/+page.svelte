830eb5f4596ec85f10d28a1578b9ce050b99643f
QuantumKya <rohan.vats4nier@gmail.com>
2025-08-29 20:34:07 -0500

    Prepare for gh-pages

--- src/routes/pixelart/+page.svelte
+++ src/routes/pixelart/+page.svelte
@@ -1,3 +1,6 @@
 <div class="pl-5 pr-3 pt-3 relative bg-linear-to-b from-green-600 to-[100vh] to-green-950 min-h-[100vh]">
+    <h1 class="text-6xl">pixel art</h1>
+    <p>Music I've made.</p>
+
     <img src="/pixelart/ricewyellowcurry.png" alt="Curry" class="cool-image w-[150px]">
 </div>
