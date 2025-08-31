5ae6d612b4d9206fdee30184480fd0e346452cca
QuantumKya <rohan.vats4nier@gmail.com>
2025-08-31 10:07:38 -0500

    added gaster dance :3

--- src/routes/pixelart/+page.svelte
+++ src/routes/pixelart/+page.svelte
@@ -1,6 +1,15 @@
+
+<svelte:head>
+    <style>
+        body {
+            image-rendering: pixelated;
+        }
+    </style>
+</svelte:head>
+
 <div class="pl-5 pr-3 pt-3 relative bg-linear-to-b from-green-600 to-[100vh] to-green-950 min-h-[100vh]">
     <h1 class="text-6xl">pixel art</h1>
-    <p>Music I've made.</p>
+    <p>Pixel art I've made.</p>
 
-    <img src="/pixelart/ricewyellowcurry.png" alt="Curry" class="cool-image w-[150px]">
+    <img src="/pixelart/gaster_dancer.gif" alt="Curry" class="w-[200px]">
 </div>
