757db378cf9e8d107f42747fabab56288234dfad
QuantumKya <rohan.vats4nier@gmail.com>
2025-09-22 00:40:53 -0500

    Updates

---
+++ _app/immutable/chunks/y0c4LWMu.js
@@ -0,0 +1,1 @@
+import{at as a}from"./ZqR8EKJB.js";a();
--- _app/immutable/nodes/5.lmUy9fqm.js
+++
@@ -1,3 +0,0 @@
-import"../chunks/DsnmJJEf.js";import"../chunks/69_IOA4Y.js";import{f as r,X as p,b as e}from"../chunks/COrX4Tl-.js";var l=r(`<style>body {
-            image-rendering: pixelated;
-        }</style>`),s=r(`<div class="pl-5 pr-3 pt-3 relative bg-linear-to-b from-green-600 to-[100vh] to-green-950 min-h-[100vh]"><h1 class="text-6xl">pixel art</h1> <p>Pixel art I've made.</p> <img src="/pixelart/gaster_dancer.gif" alt="Curry" class="w-[200px]"/></div>`);function d(a){var t=s();p(i=>{var o=l();e(i,o)}),e(a,t)}export{d as component};
